d82433c3ee49a9ba19c4098fdaeda9230bbe5eee
Lillie Rugtveit <75392499+iLillie@users.noreply.github.com>
2022-05-05 15:02:17 +0200

    Add PostgreSQL Database and EFCore

--- Celestia.Api/Program.cs
+++ Celestia.Api/Program.cs
@@ -1,9 +1,13 @@
+using Celestia.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddCors(options => options.AddPolicy("All", build => build.AllowAnyHeader()
     .AllowAnyOrigin()
     .AllowAnyMethod()));
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
 var app = builder.Build();
 
 
---
+++ Celestia.Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Celestia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Celestia.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Position> Positions { get; set; }
+}
