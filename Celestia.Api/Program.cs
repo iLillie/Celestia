07425ad910ef9945c70be45e0c465523ab7103d3
Lillie Rugtveit <75392499+iLillie@users.noreply.github.com>
2022-06-02 14:53:53 +0200

    Add Auth0 Setup

--- Celestia.Api/Program.cs
+++ Celestia.Api/Program.cs
@@ -1,18 +1,17 @@
+using System.Security.Claims;
 using System.Text.Json.Serialization;
 using Celestia.Api.Configuration;
 using Celestia.Api.Interfaces;
 using Celestia.Api.Services;
 using Celestia.Data;
 using Celestia.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    options.JsonSerializerOptions.WriteIndented = true;
-});
 
 builder.Services.AddCors(options => options.AddPolicy("All", build => build.AllowAnyHeader()
     .AllowAnyOrigin()
@@ -22,6 +21,32 @@ builder.Services.Configure<PostgresConfiguration>(builder.Configuration.GetSecti
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["Auth0:Domain"];
+    options.Audience = builder.Configuration["Auth0:Audience"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = ClaimTypes.NameIdentifier
+    };
+});
+
+builder.Services.AddControllers(options => 
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+}).AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo()
@@ -29,6 +54,24 @@ builder.Services.AddSwaggerGen(c =>
         Title = "Celestia API",
         Version = "v1"
     });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                Scopes = new Dictionary<string, string>
+                {
+                    { "openid", "Open Id" }
+                },
+                AuthorizationUrl = new Uri(builder.Configuration["Auth0:Domain"] + "authorize?audience=" + builder.Configuration["Auth0:Audience"])
+            }
+        }
+    });
+    c.OperationFilter<SecurityRequirementsOperationFilter>();
 });
 
 builder.Services.ResolveDependencies();
@@ -39,12 +82,20 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+        c.OAuthClientId(builder.Configuration["Auth0:ClientId"]);
+    });
     app.UseDeveloperExceptionPage();
 }
 
 app.UseCors("All");
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseRouting();
 app.MapDefaultControllerRoute();
+
+
 
 app.Run();
